b19541963955aa9ad8c780b2b5401ebdbc0b849d
8 Gianfortoni <pwgianfortoni@gmail.com>
2021-04-26 17:39:57 -0700

    Added wizard, trance level and statuses.

--- src/progressbar.ts
+++ src/progressbar.ts
@@ -4,29 +4,43 @@ export class ProgressBar {
   progress: number
   stage?: createjs.Stage
   removeOnLoad: boolean
-  constructor(stage: createjs.Stage, removeOnLoad: boolean) {
+  constructor(stage?: createjs.Stage, removeOnLoad?: boolean, x?: number, y?: number, width?: number, height?: number) {
     this.outerBar = new createjs.Shape()
     this.innerBar = new createjs.Shape()
-    this.outerBar.graphics.beginFill("#181818").drawRoundRectComplex(0, 0, 400, 60, 5, 5, 5, 5)
-    this.outerBar.x = 200
-    this.outerBar.y = 270
+    if (!width || !height) {
+      width = 400
+      height = 60
+    }
+    if (!x || !y) {
+      x = 200
+      y = 270
+    }
+    this.outerBar.graphics.beginFill("#181818").drawRoundRectComplex(0, 0, width, height, 5, 5, 5, 5)
+    this.outerBar.x = x
+    this.outerBar.y = y
     this.progress = 0
-    stage.addChild(this.outerBar)
+    if (stage) {
+      stage.addChild(this.outerBar)
+    }
 
-    this.innerBar.graphics.beginFill("#327fa8").drawRect(0, 0, 380, 40)
-    this.innerBar.x = 210
-    this.innerBar.y = 280
+    this.innerBar.graphics.beginFill("#327fa8").drawRect(0, 0, width - 20, height - 20)
+    this.innerBar.x = x + 10
+    this.innerBar.y = y + 10
     this.innerBar.scaleX = this.progress
 
-    stage.addChild(this.innerBar)
+    if (stage) {
+      stage.addChild(this.innerBar)
+    }
     this.stage = stage
-    this.removeOnLoad = removeOnLoad
+    this.removeOnLoad = removeOnLoad || false
   }
   handleProgress(event: Object) {
     var progressEvent = <createjs.ProgressEvent>event
     this.progress = progressEvent.progress
     this.innerBar.scaleX = this.progress
-    this.stage!.update()
+    if (this.stage) {
+      this.stage.update()
+    }
   }
   remove() {
     if (this.stage) {
